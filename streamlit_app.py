082ba8f7423583b984c3f706eff2ca77b21f4855
Sanjay Kawathia <113019966+skawathia@users.noreply.github.com>
2022-09-07 15:36:07 +0530

    Update streamlit_app.py
    streamlit.text('Omega 3 and blueberry oatmeal')
    streamlit.text('Kale, spinach & Rocket Smoothie')
    streamlit.text('Hard-Boiled Free-Range Egg')

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy dinner')
 streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('Kale, spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
